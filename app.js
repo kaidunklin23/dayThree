8017df401e7cab576f2edab125364dbda003d22f
Kaija Dunklin <kaykay_s@yahoo.com>
2020-07-15 20:26:58 -0400

    today

---
+++ app.js
@@ -0,0 +1,35 @@
+kArray = [["chicken",1], ["nugget",2]];
+
+var mArray = [
+    ["mike",2], ["flower",4], ["nest",6]
+  ];
+  for (var k=0; k < mArray.length; k++) {
+    for (var j=0; j < mArray[k].length; j++) {
+      console.log(mArray[k][j]);
+    }
+  }
+  petArray = [ [ ["fish",2] , ["dog", 4 ] , [ ["hamster" , 6] , [ "snake",8] ] , [ "cat",10 ] ];
+
+  function greaterThan(val1,val2) {
+    if (val > val2) {
+     
+      return val1;
+    }
+    else (val < val2) {
+     
+      return val2;
+    }}
+
+  console.log(greaterThan);
+
+  chicken =1;
+    duck=[1];
+
+    function theyEqual(chicken, duck) {
+    if(chicken !== duck) {
+        return(theyEqual) = "They are equal";
+    } else {
+        return(theyEqual) = "they are not equal";
+    }
+}
+console.log(theyEqual());
